570a3ce816e96dc08f61eac52fc6f345fa7a286c
juniorfrogie <penvoneat@gmail.com>
2018-10-10 14:40:10 +0700

    Display questions

--- app/Question.php
+++ app/Question.php
@@ -8,12 +8,35 @@ class Question extends Model
 {
     protected $fillable = ['title','content'];
 
-    public function user(){
+    public function user()
+    {
       return $this->belongsTo('App\User');
     }
 
-    public function setTitleAttribute($value){
+    public function setTitleAttribute($value)
+    {
       $this->attributes['title'] = $value;
       $this->attributes['slug'] = str_slug($value);
+    }
+
+    public function getUrlAttribute()
+    {
+      return route("questions.show", $this->id);
+    }
+
+    public function getCreatedDateAttribute()
+    {
+      return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute()
+    {
+      if ($this->answers > 0) {
+        if ($this->best_answer_id) {
+          return "answered-accepted";
+        }
+        return "answered";
+      }
+      return "unanswered";
     }
 }
